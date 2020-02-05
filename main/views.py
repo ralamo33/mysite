5986b33ee7c973651407f6315d7d48bfe023184a
ralamo33 <ralamo33@gmail.com>
2020-02-05 00:20:42 +0000

    Sytle file works properly.

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Project
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 
 # Create your views here.
@@ -16,11 +17,13 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New Account Created: {username}")
             login(request, user)
             return redirect("main:homepage")
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}: {form.error_messages}")
 
 
     form = UserCreationForm
