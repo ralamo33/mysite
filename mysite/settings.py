18e9d9eb0ac6347c041ff13ac10a4b3522ffc3eb
ralamo33 <ralamo33@gmail.com>
2020-02-10 21:30:46 +0000

    Fixing runtime

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#z_c_k+@90@j*i-d*45%*ymbx*3iy%+xoqcryzrt0$d&qeq8&@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
