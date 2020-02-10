1bfb3db718a12f191b64b361a279be67be710439
ralamo33 <ralamo33@gmail.com>
2020-02-10 21:04:03 +0000

    Deploying to Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#z_c_k+@90@j*i-d*45%*ymbx*3iy%+xoqcryzrt0$d&qeq8&@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ralamo33.pythonanywhere.com",
-                 "127.0.0.1"]
+                 "127.0.0.1",
+                 "secret-hamlet-17163"]
 
 
 # Application definition
